6e1aacc4019f5c1b79a424d774a70846b5d3398d
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-11-05 09:59:33 +0100

    phpmaj

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -3,7 +3,7 @@
 $str ="Les choses que l'on
 possede finissent par nous posseder.";
 
-function mystrrev($str){
+/*function mystrrev($str){
     $r = '';
     for ($i = mb_strlen($str); $i>=0; $i--) {
         $r .= mb_substr($str, $i, 1);
@@ -11,5 +11,10 @@ function mystrrev($str){
     return $r;
 }
 $str2 = mystrrev($str);
-echo $str2;
-?>
+echo $str2;*/
+
+for($i=0; isset($str[$i]) == true; $i++);
+for($i = $i = -1;isset($str[$i]) ; $i--){
+echo $str[$i];
+}
+?>k
